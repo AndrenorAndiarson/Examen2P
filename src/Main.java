a819f1b62f13e107eb34552ad4601df2c4ed1fa9
AndrenorAndiarson <shaadowsong@hotmail.com>
2023-10-12 10:11:46 -0400

    final

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Suma sum1 = new Suma();
+        sum1.calcularSuma(1855,52);
+        Suma sum4 = new Suma();
+        sum4.calcularSuma(123,456,789);
+        Suma sum2 = new Suma();
+        sum2.calcularSuma(18.5,72.5);
+        Suma sum3 = new Suma();
+        sum3.calcularSuma(192.5,91.75,125.25);
+
+    }
+}
